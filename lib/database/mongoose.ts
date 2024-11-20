f9c3c52e4ca042d15304e0977e43f83db1b12284
202120449 <202120449@aau.edu.jo>
2024-11-20 03:02:22 +0300

    db name updated

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -23,7 +23,7 @@ export const connectToDatabase = async () => {
   cached.promise = 
     cached.promise || 
     mongoose.connect(MONGODB_URL, { 
-      dbName: 'IMAGIFY', bufferCommands: false 
+      dbName: 'imagify', bufferCommands: false 
     })
 
   cached.conn = await cached.promise;
